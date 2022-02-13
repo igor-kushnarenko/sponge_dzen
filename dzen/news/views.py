fec3acd354232eb7315b8b4969c3c715b4882205
maxim <anapa.igor@yandex.ru>
2022-02-13 19:48:22 +0300

    -add base page, index page, css;

--- dzen/news/views.py
+++ dzen/news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class Index(View):
+    def get(self, request):
+        context = {'title': 'Главное на сегодня'}
+        return render(request, 'news/index.html', context)
